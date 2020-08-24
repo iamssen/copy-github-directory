43aca45d3dd546270994a5a8d60eeb79810d2cdf
SSen <i@ssen.name>
2020-08-24 20:40:47 +0900

    feat: use GITHUB_TOKEN env to authorization

--- source/src/generate-github-directory/index.ts
+++ source/src/generate-github-directory/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import gitUrlParse, { GitUrl } from 'git-url-parse';
 import got from 'got';
-import fetch from 'node-fetch';
+import fetch, { HeadersInit } from 'node-fetch';
 import * as path from 'path';
 import { Stream } from 'stream';
 import tar from 'tar';
@@ -13,26 +13,33 @@ export interface GenerateGithubDirectoryParams {
   readonly url: string;
   readonly targetDirectory?: string;
   readonly cwd?: string;
+  readonly githubToken?: string;
 }
 
 export async function generateGithubDirectory({
   url,
   targetDirectory,
   cwd = process.cwd(),
+  githubToken = process.env.GITHUB_TOKEN,
 }: GenerateGithubDirectoryParams): Promise<string> {
   const { name, owner, filepath, filepathtype, ref: _ref }: GitUrl = gitUrlParse(url);
+
+  const headers: HeadersInit | undefined =
+    typeof githubToken === 'string' ? { Authorization: `token ${githubToken}` } : undefined;
 
   const ref: string =
     _ref.length > 0
       ? _ref
-      : await fetch(`https://api.github.com/repos/${owner}/${name}`)
+      : await fetch(`https://api.github.com/repos/${owner}/${name}`, { headers })
           .then((res) => res.json())
           .then(({ default_branch }) => default_branch);
 
   const isDirectory: boolean =
     filepathtype === ''
       ? true
-      : await fetch(`https://api.github.com/repos/${owner}/${name}/contents/${filepath}?ref=${ref}`)
+      : await fetch(`https://api.github.com/repos/${owner}/${name}/contents/${filepath}?ref=${ref}`, {
+          headers,
+        })
           .then((res) => res.json())
           .then((contents) => Array.isArray(contents));
 
